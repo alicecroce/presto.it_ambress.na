e156fac75c3a354a643388fb2dd0c16be8bb0fbc
giulia volpe <giuliavolpe@hotmail.it>
2023-06-26 20:15:22 +0200

    inizializzato vite e template

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Presto.it</title>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body>
+    {{$slot}}
+  </body>
+</html>
