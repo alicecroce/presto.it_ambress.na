7bb3bef5925ebef3120d42543f385917a5509c99
alicecroce <alicecroce2@gmail.com>
2023-06-28 12:57:10 +0200

    nav in fase di lavorazione

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,22 +1,26 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/" title="e-commerce">
+            <picture>
+                <img src="/public/img/prestoit-logo-con-txt.svg" alt="logo presto.it">
+            </picture>
+
+
+
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
+
+            <input class="form-control me-4" type="search" placeholder="cerca su Presto" aria-label="Search">
+
+            <div class="d-flex me-3 nav-item dropdown" role="button">
+                <button class="btn btn-outline-success" type="submit">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Accedi o registrati
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
@@ -26,15 +30,11 @@
                         </li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                </li>
-            </ul>
+                </button>
 
-            <input class="form-control me-4" type="search" placeholder="cerca su Presto" aria-label="Search">
+            </div>
 
-            <form class="d-flex me-3" role="button">
-                <button class="btn btn-outline-success" type="submit">Accedi o registrati</button>
-            </form>
-            <!--sono in dubbio se i type siano necessari -->
+
             <div class="d-flex">
                 <a class="btn btn-outline-success" role="button" href="#">Inserisci un annuncio</a>
             </div>
