a59c7b245c3bce86b93c656794be83d35faa49d3
alicecroce <alicecroce2@gmail.com>
2023-07-01 23:13:05 +0200

    Creazione footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,52 @@
+    <footer id="footer">
+
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Presto.it</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet. <br>
+                            Napoli, NA 90123<br>
+                            Torino <br><br>
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@presto.it<br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Link utili</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>I nostri servizi</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Tutti gli annunci </a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Registrati</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Accedi</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-4">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Presto.it</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+            </a> Designed by Ambress.na group per Aulab srl</a>
+        </div>
+        </div>
+    </footer>
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,23 @@
 <x-main>
 
     <x-hero />
-    
+
     <div id="success-message">
         @if (session('success'))
-        <div class="m-2 p-2 d-flex justify-content-center">
-            <span class="fw-bold bg-successo p-2 text-successo bg-opacity-50 border border-successo rounded">
-                {{ session('success') }}</span>
-        </div>
+            <div class="m-2 p-2 d-flex justify-content-center">
+                <span class="fw-bold bg-successo p-2 text-successo bg-opacity-50 border border-successo rounded">
+                    {{ session('success') }}</span>
+            </div>
         @endif
     </div>
 
     <div class="container parent my-5">
         @foreach ($categories as $category)
-            <a class="text-center category-box text-white d-flex justify-content-center align-items-center {{ $category->slug}}-color"
+            <a class="text-center category-box text-white d-flex justify-content-center align-items-center {{ $category->slug }}-color"
                 href="{{ route('categoryshow', compact('category')) }}">{{ ucFirst($category->name) }}</a>
         @endforeach
     </div>
+
+    <x-footer />
 
 </x-main>
