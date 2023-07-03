edf31777cad144df5a9f8fe3d755b74773be7fb5
giulia volpe <giuliavolpe@hotmail.it>
2023-07-03 20:45:51 +0200

    aggiustata paginate

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class FrontController extends Controller
 {
     public function searchAdvs(Request $request) {
-        $advs = Adv::search($request->searched)->get();
+        $advs = Adv::search($request->searched)->paginate(5);
         return view('adv.index', compact('advs'));
     }
 }
