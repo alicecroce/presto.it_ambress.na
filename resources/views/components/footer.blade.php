51e6245f1ac54f83f4d5d40f712174f535a17597
alicecroce <alicecroce2@gmail.com>
2023-07-03 12:09:41 +0200

    Icone caricate, non soddisfano, ma sono segnaposto

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -37,11 +37,11 @@
                         <h4>Our Social Networks</h4>
                         <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
                         <div class="social-links mt-3">
-                          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                          <a href="#" class="twitter"><i class="bi bi-google"></i></a>
+                          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                          <a href="#" class="google-plus"><i class="bi bi-skype"></i></i></a>
+                          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
                         </div>
                       </div>
 
