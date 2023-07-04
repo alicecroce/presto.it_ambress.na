e0017baed791be12a73519df4b92b0558a92ecf3
giulia volpe <giuliavolpe@hotmail.it>
2023-07-04 21:50:05 +0200

    spostati bottoni

--- resources/views/revisor/rejected.blade.php
+++ resources/views/revisor/rejected.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             @forelse ($advs as $adv)
                 <div class="col-12 col-md-3">
-                    <div class="card my-3" style="height: 36rem;">
+                    <div class="card my-3" style="height: 36rem; width: 18rem;">
 
                         <img src="https://placehold.co/300/6230A3/FFFFFF/png" class="card-img-top" alt="img placeholder">
                         <div class="card-body position-relative">
@@ -14,7 +14,7 @@
                                 <p class="card-text">{{ $adv->abstract }}</p>
                             </div>
 
-                            <div class="row">
+                            <div class="row mt-3">
                                 <div class="col border-top">
                                     {{ $adv->getCategory() }}
                                 </div>
@@ -23,18 +23,18 @@
                                 </div>
                             </div>
 
-                            <div class="row">
-                                <div class="col-6">
+                            <div class="row mt-2 g-0">
+                                <div class="col-6 g-0">
                                     <form action="{{ route('revisor.accept_adv', ['adv' => $adv]) }}" method="POST">
                                         @csrf
                                         @method('PATCH')
-                                        <button type="submit" class="btn btn-success shadow">Accetta</button>
+                                        <button type="submit" class="btn btn-accedi ">Accetta</button>
                                     </form>
                                 </div>
 
-                                <div class="col-6">
+                                <div class="col-6 g-0">
                                     <a href="{{ route('adv.show', ['adv' => $adv['id']]) }}"
-                                        class="btn btn-show position-absolute bottom-0 end-0 m-3">Vedi
+                                        class="btn btn-accedi text-nowrap">Vedi
                                         Annuncio</a>
                                 </div>
                             </div>
