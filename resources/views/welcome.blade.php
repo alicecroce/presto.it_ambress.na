6ffb9fe717175579efddd3f3cd5593e427c5ae1a
Giuseppe Maria Arnone <eppesuigenonra@gmail.com>
2023-07-04 18:39:08 +0200

    little bugfix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 
     <div id="success-message">
         @if (session('success'))
-            <div class="d-flex  a justify-content-center">
-                <div class="alert alert-success w-75 lign-items-center text-center" role="alert">
+            <div class="d-flex justify-content-center">
+                <div class="alert alert-success w-75 align-items-center text-center" role="alert">
                     <span class="bi bi-check-circle m-2"></span>
                     messaggio di successo
                     {{ session('success') }}
