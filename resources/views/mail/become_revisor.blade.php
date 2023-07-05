e40415e84304a24a3f69a8d55fa46001ba22ee4a
Mari-Fallacara <fallma@outlook.it>
2023-07-05 20:02:53 +0200

    template mailtrap completato

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -6,16 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Presto.it</title>
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
     <div>
-        <h1>Un utente ha chiesto di lavorare con noi</h1>
-        <h2>Dati personali</h2>
-        <p>Nome: {{ $user->name }}</p>
-        <p>Cognome: {{ $user->surname }}</p>
-        <p>Email: {{ $user->email }}</p>
-        <p>Se vuoi autorizzare l'utente clicca qui:</p>
+        <h1 id="tem-email"> RICHIESTA REVISORE </h1>
+        <h2 id="tem-email"> {{ $user->name }} ha chiesto di lavorare con noi!</h2>
+        <h3><b>Dati personali</b></h3>
+        <p style="">Nome: <em>{{ $user->name }}</em></p>
+        <p>Cognome: <em>{{ $user->surname }}</em></p>
+        <p>Email:<em>{{ $user->email }}</em></p>
+        <p><b>Se vuoi autorizzare l'utente clicca qui:</b></p>
         <a href="{{ route('make.revisor', compact('user')) }}">Rendi revisore</a>
     </div>
 </body>
