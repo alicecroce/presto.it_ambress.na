2a6095fc37e2886bc7cc79fbd9fb125793308144
giulia volpe <giuliavolpe@hotmail.it>
2023-07-06 19:43:10 +0200

    messo btn modifica annuncio user

--- resources/views/adv/edit.blade.php
+++ resources/views/adv/edit.blade.php
@@ -1,4 +1,4 @@
 <x-main>
-    <h1>Modifica il tuo annuncio</h1>
+    <h1 id="categoryName" class="text-center my-3">Modifica il tuo annuncio</h1>
     <livewire:adv-edit-form :adv="$adv"/>
 </x-main>
