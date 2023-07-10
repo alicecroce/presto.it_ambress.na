712e7a7db343015b38b6943968a1922c0daf9480
alicecroce <alicecroce2@gmail.com>
2023-07-10 18:53:33 +0200

    installato spatie image

---
+++ app/Jobs/ResizeImage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use Spatie\Image\Image;
+use Illuminate\Bus\Queueable;
+use Spatie\Image\Manipulations;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class ResizeImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $w;
+    private $h;
+    private $fileName;
+    private $path;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($filePath, $w, $h)
+    {
+        $this->path = dirname($filePath);
+        $this->path = basename($filePath);
+        $this->w = $w;
+        $this->h = $h;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
+        $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+
+        $croppedImage=Image::load($srcPath)
+        ->crop(Manipulations::CROP_CENTER, $w, $h)
+        ->save($destPath)
+    }
+}
