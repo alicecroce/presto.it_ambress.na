d341fd7074ed21432a1055ed90ecd0232cb9ba5d
alicecroce <alicecroce2@gmail.com>
2023-07-11 21:01:00 +0200

    Uomo delle caverne, errore 404, completato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -64,7 +64,7 @@
                         <h4>{{ __('ui.ourSocial') }}</h4>
                         <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
                         <div class="social-links mt-3">
-                            <a href="{{ route('lostinspace') }}">
+                            <a href="{{ route('mancave') }}">
                                 <box-icon type='logo' name='twitter' color='#ffffff'></box-icon>
                             </a>
                             <a href="#">
