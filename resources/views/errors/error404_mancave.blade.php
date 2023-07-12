57dd320aea4f8be0f795613c1ed658b338c973a9
alicecroce <alicecroce2@gmail.com>
2023-07-12 19:06:31 +0200

    page 500 prob

--- resources/views/errors/error404_mancave.blade.php
+++ resources/views/errors/error404_mancave.blade.php
@@ -1,10 +1,10 @@
 <x-main>
 
-    <section class="page_404">
-        <div class="container">
+    <section class="page_404 ">
+        <div class="container ">
             <div class="row ">
                 <div class="col-sm-12 ">
-                    <div class="col-sm-10 col-sm-offset-1  text-center">
+                    <div class="col-sm-offset-1  text-center">
                         <div class="four_zero_four_bg">
                             <h1 class="text-center ">404</h1>
 
