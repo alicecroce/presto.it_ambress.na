d7b71ca8ae789f66500b745e9c4da384c4928c37
Giuseppe Maria Arnone <eppesuigenonra@gmail.com>
2023-07-13 19:29:15 +0200

    adding home routes in errors pages

--- resources/views/errors/error403_lotr.blade.php
+++ resources/views/errors/error403_lotr.blade.php
@@ -1,19 +1,21 @@
 <x-main>
     
     <div class="gandalfbody">
-        <div class="gandalf">
-            <div class="fireball"></div>
-            <div class="skirt"></div>
-            <div class="sleeves"></div>
-            <div class="shoulders">
-                <div class="hand left"></div>
-                <div class="hand right"></div>
+        <a href="/">
+            <div class="gandalf">
+                <div class="fireball"></div>
+                <div class="skirt"></div>
+                <div class="sleeves"></div>
+                <div class="shoulders">
+                    <div class="hand left"></div>
+                    <div class="hand right"></div>
+                </div>
+                <div class="head">
+                    <div class="hair"></div>
+                    <div class="beard"></div>
+                </div>
             </div>
-            <div class="head">
-                <div class="hair"></div>
-                <div class="beard"></div>
-            </div>
-        </div>
+        </a>
         <div class="message">
             <h1>403 - You Shall Not Pass</h1>
             <p>Uh oh, Gandalf is blocking the way!<br />Maybe you have a typo in the url? Or you meant to go to a
