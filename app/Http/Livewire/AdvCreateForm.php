39f42855e0dc0e725fc060af672786fc14a79764
Giuseppe Maria Arnone <eppesuigenonra@gmail.com>
2023-07-17 20:36:19 +0200

    US 8 100%

--- app/Http/Livewire/AdvCreateForm.php
+++ app/Http/Livewire/AdvCreateForm.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\File;
 use App\Http\Requests\StoreAdvRequest;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\RemoveFaces;
 use Illuminate\Contracts\Session\Session;
 
 class AdvCreateForm extends Component
@@ -77,9 +78,13 @@ class AdvCreateForm extends Component
                 $newFileName = "advs/{$this->adv->id}";
                 $newImage = $this->adv->images()->create(['path' => $image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
