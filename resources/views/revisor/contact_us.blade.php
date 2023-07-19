e6d0d787308ddc311e7f2a95541d365f2322c2a2
Giuseppe Maria Arnone <eppesuigenonra@gmail.com>
2023-07-19 19:42:03 +0200

    ucfirst in giro piu vari fix

--- resources/views/revisor/contact_us.blade.php
+++ resources/views/revisor/contact_us.blade.php
@@ -10,7 +10,7 @@
     </div>
 
     <div class="container-fluid ps-md-0">
-        
+
         <div class="row g-0">
             <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image opacity-75 "></div>
             <div class="col-md-8 col-lg-6">
@@ -25,98 +25,75 @@
                                     <x-locale locale lang="en" nation="gb" />
                                 </li>
                             </ul>
-                        <div class="row">
-                            <div class="d-flex align-items-center">
-                                <a href="/" class="text-center">
-                                    <img class="img-fluid w-50"
-                                        src="{{ asset(Storage::url('public/img/prestoit-logo_alpha.png')) }}"
-                                        alt="logo presto.it">
-                                </a>
+                            <div class="row">
+                                <div class="d-flex align-items-center">
+                                    <a href="/" class="text-center">
+                                        <img class="img-fluid w-50"
+                                            src="{{ asset(Storage::url('public/img/prestoit-logo_alpha.png')) }}"
+                                            alt="logo presto.it">
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-9 col-lg-8 mx-auto">
-                                <h3 class="login-heading mb-4">{{__('ui.revForm')}}</h3>
+                            <div class="row">
+                                <div class="col-md-9 col-lg-8 mx-auto">
+                                    <h3 class="login-heading mb-4">{{ __('ui.revForm') }}</h3>
 
-                                <!-- Sign In Form -->
-                                <form action="{{ route('savecontact.revisor') }}" method="POST">
-                                    @csrf
-                                    @method('POST')
+                                    <!-- Sign In Form -->
+                                    <form action="{{ route('savecontact.revisor') }}" method="POST">
+                                        @csrf
+                                        @method('POST')
 
-                                    @if ($errors->any())
-                                        <div class="alert alert-danger">
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
+                                        @if ($errors->any())
+                                            <div class="alert alert-danger">
+                                                <ul>
+                                                    @foreach ($errors->all() as $error)
+                                                        <li>{{ $error }}</li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        @endif
+
+                                        <div class="d-flex d-inline">
+                                            <div class="form-floating flex-grow-1 mb-3 me-2">
+                                                <input readonly type="text" value="{{ Auth::user()->name }}"
+                                                    name="name" class="form-control" id="name"
+                                                    placeholder="Gino">
+                                                <label for="name">{{ __('ui.name') }}</label>
+                                            </div>
+                                            <div class="form-floating flex-grow-1 mb-3 ">
+                                                <input readonly type="text" value="{{ Auth::user()->surname }}"
+                                                    name="surname" class="form-control" id="surname"
+                                                    placeholder="Gini">
+                                                <label for="surname">{{ __('ui.surname') }}</label>
+                                            </div>
                                         </div>
-                                    @endif
 
-                                    <div class="d-flex d-inline">
-                                        <div class="form-floating flex-grow-1 mb-3 me-2">
-                                            <input readonly type="text" value="{{ Auth::user()->name }}"
-                                                name="name" class="form-control" id="name" placeholder="Gino">
-                                            <label for="name">{{__('ui.name')}}</label>
+                                        <div class="form-floating mb-3">
+                                            <input type="email" value="{{ Auth::user()->email }}" name="email"
+                                                class="form-control" id="email" placeholder="name@example.com">
+                                            <label for="email">{{ __('ui.emailAdd') }}</label>
                                         </div>
-                                        <div class="form-floating flex-grow-1 mb-3 ">
-                                            <input readonly type="text" value="{{ Auth::user()->surname }}"
-                                                name="surname" class="form-control" id="surname" placeholder="Gini">
-                                            <label for="surname">{{__('ui.surname')}}</label>
+
+                                        <div class="mb-3">
+                                            <textarea style="height: 8rem;" name="description" class="form-control" placeholder="{{ __('ui.whyRev') }}"
+                                                id="description"></textarea>
                                         </div>
-                                    </div>
-
-                                    <div class="form-floating mb-3">
-                                        <input type="email" value="{{ Auth::user()->email }}" name="email"
-                                            class="form-control" id="email" placeholder="name@example.com">
-                                        <label for="email">{{__('ui.emailAdd')}}</label>
-                                    </div>
-
-                                    <div class="mb-3">
-                                        <textarea style="height: 8rem;" name="description" class="form-control"
-                                            placeholder="{{__('ui.whyRev')}}"
-                                            id="description"></textarea>
-                                    </div>
 
 
 
 
-                                    <div class="d-grid">
-                                        <button class="btn btn-lg btn-login text-uppercase fw-bold mb-2 btn-cerca"
-                                            type="submit">{{__('ui.apply')}}</button>
-                                    </div>
+                                        <div class="d-grid">
+                                            <button class="btn btn-lg btn-login text-uppercase fw-bold mb-2 btn-cerca"
+                                                type="submit">{{ __('ui.apply') }}</button>
+                                        </div>
 
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </x-main>
